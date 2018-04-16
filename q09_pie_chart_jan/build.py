09eba8119f5e5caa1d2aee0cd85db798b3dffc36
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-04-16 17:13:42 +0000

    Done

--- q09_pie_chart_jan/build.py
+++ q09_pie_chart_jan/build.py
@@ -3,13 +3,16 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
+from q06_sub_total.build import q06_sub_total
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 
 def q09_pie_chart_jan(path1,path2):
 
-    "write your solution here"
+    'write your solution here'
+    df_sub =q06_sub_total(path1,path2)
+
+    return df_sub['Jan'].plot(kind='pie')
 
 
 
