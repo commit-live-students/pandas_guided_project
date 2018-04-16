9356357c99d725b77b111f4780a6ff1e3eabeb2e
greyatomtestuser2 <greyatomtestuser2@gmail.com>
2018-04-16 16:28:11 +0000

    Done

--- q06_sub_total/build.py
+++ q06_sub_total/build.py
@@ -3,14 +3,16 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import sys
 import os
-#sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q05_replace_missing_values.build import q05_replace_missing_values
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+from q05_replace_missing_values.build import q05_replace_missing_values
 
 path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q06_sub_total(path1,path2):
     "write your solution here"
+    df_final = q05_replace_missing_values(path1,path2)
+    #df_final['abbr'] = df_final['abbr'].astype(str)
+    df_sub=df_final[["abbr", "Jan", "Feb", "Mar", "total"]].groupby("abbr").sum()
 
-
-
+    return df_sub
--- q07_symbols/build.py
+++ q07_symbols/build.py
@@ -4,15 +4,13 @@ from sklearn.model_selection import train_test_split
 import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
+from q06_sub_total.build import q06_sub_total
 
 path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q07_symbols(path1,path2):
     "write your solution here"
-
-
-
-#print(q07_symbols(path1,path2))
-
+    df_sub = q06_sub_total(path1,path2)
+    formatted_df = df_sub.applymap(lambda x: "${:,.0f}".format(x))
+    return formatted_df
