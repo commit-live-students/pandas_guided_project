71a8f8ed3178e38080abc7677f15870e00a80b78
abkedar <anupbhande@gmail.com>
2018-04-20 07:34:01 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,11 @@
 import pandas as pd
 
-
+path = 'data/excel-comp-data.xlsx'
 def q01_load_data(path):
-    "write your solution here"
+    'write your solution here'
+    df = pd.read_excel(path)
+    df['total'] = pd.to_numeric(df['Jan']) + pd.to_numeric(df['Feb']) + pd.to_numeric(df['Mar'])
+    return (df)
+q01_load_data(path)
+
+
