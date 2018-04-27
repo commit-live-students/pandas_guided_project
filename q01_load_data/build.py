3bafd81eee670d01fe058dc933783c584e829da5
PBPatil <theprasadpatil@gmail.com>
2018-04-27 16:12:08 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,22 @@
+# %load q01_load_data/build.py
 import pandas as pd
 
-
+path = 'data/excel-comp-data.xlsx'
 def q01_load_data(path):
-    "write your solution here"
+    df = pd.read_excel(path)
+    df['state']=[x.lower() for x in df['state']]
+    df['total'] = df['Jan']+df['Feb']+df['Mar']
+    return df
+path = 'data/excel-comp-data.xlsx'
+df = pd.read_excel(path)
+df.state.value_counts()
+df.columns.values
+df['state']=[x.lower() for x in df['state']]
+df['total'] = df['Jan']+df['Feb']+df['Mar']
+df['total'].head()
+df['Mar'].head()
+df.columns.values
+df.shape
+type(df)
+
+
