9ad0527e758c64b01e230fc6a91269bfffaa64b9
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-05-17 14:50:11 +0000

    Done

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import sys, os
-#sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
-
-
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+#from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
+from q01_load_data.build import q01_load_data
+path = 'data/excel-comp-data.xlsx'
 def q02_append_row(path):
-    "write your solution here"
-
-
-
-
+    df = q01_load_data(path)
+    df1 = df[['Jan', 'Feb', 'Mar', 'total']].sum(axis=0)
+    df1 = pd.DataFrame(df1)
+    df2 = pd.DataFrame(columns=['Jan', 'Feb', 'Mar', 'total'])
+    df2['Jan'] = df1.loc['Jan']
+    df2['Feb'] = df1.loc['Feb']
+    df2['Mar'] = df1.loc['Mar']
+    df2['total'] = df1.loc['total']
+    df3 = df.append(df2, ignore_index=True)
+    return df3
