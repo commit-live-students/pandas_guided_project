82bdd9b96f4bbf41b5525ab96fbfca7b7301a4cd
santoshmayekar <santosh.mayekar@outlook.com>
2018-05-19 07:54:48 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,12 @@
 import pandas as pd
 
-
 def q01_load_data(path):
-    "write your solution here"
+    path='data/excel-comp-data.xlsx'
+    data = pd.read_excel(path)
+    #data['state']= map(lambda x: x.lower(), data['state'])
+    df1=data['state'].str.lower()
+    data['state']=df1
+    data['total']=data['Jan']+data['Feb']+data['Mar']
+    return data
+
+
