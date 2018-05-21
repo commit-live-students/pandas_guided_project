840309b853e358a4335c5e6f4ebdc67b2df5272b
AdityaMV1215 <aditya.vadhavkar12@gmail.com>
2018-05-21 06:55:16 +0000

    Done

--- q06_sub_total/build.py
+++ q06_sub_total/build.py
@@ -3,14 +3,16 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 import sys
 import os
-#sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q05_replace_missing_values.build import q05_replace_missing_values
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+from q05_replace_missing_values.build import q05_replace_missing_values
 
 path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q06_sub_total(path1,path2):
-    "write your solution here"
+    df = q05_replace_missing_values(path1,path2)
+    #print(df)
+    df1 = df.groupby('abbr').sum()
+    return df1
 
-
-
+#q06_sub_total(path1,path2)
