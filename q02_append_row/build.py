d4d53a810254cb7638e7d5400b483743fd77680e
djocz <jocelynd789@gmail.com>
2018-07-05 08:54:29 +0000

    Done

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -6,7 +6,5 @@ from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
 
 def q02_append_row(path):
     "write your solution here"
-
-
-
-
+    df = q01_load_data(path)
+    return df.append(df.sum(numeric_only=True), ignore_index=True)
