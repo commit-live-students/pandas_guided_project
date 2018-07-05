cbedf677af907cb578d076c7b6cc4c267b768c90
djocz <jocelynd789@gmail.com>
2018-07-05 12:10:48 +0000

    Done

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -1,10 +1,17 @@
+# %load q02_append_row/build.py
 import pandas as pd
+import numpy as np
 import sys, os
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
 
 
 def q02_append_row(path):
-    "write your solution here"
+    'write your solution here'
     df = q01_load_data(path)
-    return df.append(df.sum(numeric_only=True), ignore_index=True)
+    sumn = df.sum(numeric_only=True)
+    return df.append(sumn, ignore_index=True)
+
+
+q02_append_row('data/excel-comp-data.xlsx')
+
--- q04_mapping/build.py
+++ q04_mapping/build.py
@@ -1,10 +1,18 @@
+# %load q04_mapping/build.py
 import pandas as pd
 import sys, os
 import numpy as np
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q02_append_row.build import q02_append_row
+from greyatomlib.pandas_guided_project.q03_scrape_clean.build import q03_scrape_clean
 def q04_mapping(path1,path2):
     "write your solution here"
+    df1 = q02_append_row(path1)
+    df2 = q03_scrape_clean(path2)
+    return df1,df2
 
 
 
+path1 = 'data/excel-comp-data.xlsx'
+path2 = 'data/scraped.csv'
+q04_mapping(path1,path2)
