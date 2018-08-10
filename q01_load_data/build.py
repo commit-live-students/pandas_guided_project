f5f0b40f95bf62ed0ede446a823429077099aed2
rianboy18 <pranavr95.pn@gmail.com>
2018-08-10 10:07:48 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,18 @@
+# %load q01_load_data/build.py
 import pandas as pd
+import numpy as np
+
+def q01_load_data(path='data/excel-comp-data.xlsx'):
+    #'write your solution here'
+    df=pd.read_excel(path)
+    df=pd.DataFrame(df)
+    df['state']=df['state'].str.lower()#Series column lower case syntax is str.lower()
+    df['Total']=df['Jan']+df['Feb']+df['Mar']#+ operator works coz its overloaded
+    return df
+
+c=q01_load_data(path='data/excel-comp-data.xlsx')
+c.shape
 
 
-def q01_load_data(path):
-    "write your solution here"
+
+
