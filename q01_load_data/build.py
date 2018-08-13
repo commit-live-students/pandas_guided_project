b34816614ef068e1d59426177b35ec09ae28b09c
rushabh-mehta <rushabh123@gmail.com>
2018-08-13 14:42:47 +0000

    Done

--- q01_load_data/build.py
+++ q01_load_data/build.py
@@ -1,5 +1,10 @@
+# %load q01_load_data/build.py
 import pandas as pd
 
 
 def q01_load_data(path):
-    "write your solution here"
+    df = pd.read_excel(path)
+    df['total'] = df['Jan']+df['Feb']+df['Mar']
+    return df
+
+
