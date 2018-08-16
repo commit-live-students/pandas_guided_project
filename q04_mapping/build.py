dd3835afb4a362b8351206092d212c1e37a7c9b2
rushabh-mehta <rushabh123@gmail.com>
2018-08-16 16:53:34 +0000

    Done

--- q04_mapping/build.py
+++ q04_mapping/build.py
@@ -1,10 +1,18 @@
+# %load q04_mapping/build.py
 import pandas as pd
 import sys, os
 import numpy as np
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q02_append_row.build import q02_append_row
-def q04_mapping(path1,path2):
-    "write your solution here"
 
+def q04_mapping(path1,path2):
+    df1 = pd.read_excel(path1)
+    df1['total']=df1['Jan']+df1['Feb']+df1['Mar']
+    df1.loc[len(df1),:]=df1.sum()
+    df2 = pd.read_csv(path2)
+    abbr_dict = dict(zip(df2.iloc[:,[1,6]]['United States of America'],df2.iloc[:,[1,6]]['Unnamed: 6']))
+    #df1['abbr']=np.where(df1.iloc[4]) in abbr_dict,str(abbr_dict[df1.iloc[4]]),'nan')
+    df1.iloc[:,6]=df1['state'].map(abbr_dict)
+    return df1
 
 
