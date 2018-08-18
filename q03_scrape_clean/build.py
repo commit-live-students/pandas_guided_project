6cf6e4c7a430afc5d74575731e0413e01aa005c7
Tusharshah2006 <tusharshah2006@gmail.com>
2018-08-18 04:06:30 +0000

    Done

--- q03_scrape_clean/build.py
+++ q03_scrape_clean/build.py
@@ -1,9 +1,20 @@
+# %load q03_scrape_clean/build.py
 import pandas as pd
 import sys, os
 import requests
+from bs4 import BeautifulSoup
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 
 
 def q03_scrape_clean(url):
-    "write your solution here"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content,'lxml')
+    table = soup.find_all('table')[0]
+    df = pd.read_html(str(table))[0]
+    df = df.iloc[12:,:]
+
+    return df
+
+q03_scrape_clean('https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations')
+
 
