5e9dd1d29b193814b66d8474e7e3a13326a108a7
Chinmay Chopade <cchopade@gmail.com>
2018-08-20 14:52:54 +0530

    updated with path in build.py

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -3,6 +3,8 @@ import sys, os
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
 
+path = 'data/excel-comp-data.xlsx'
+
 
 def q02_append_row(path):
     "write your solution here"
