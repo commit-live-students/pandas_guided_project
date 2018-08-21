4634db87f7f21873d76c3147629f2b80fa961e0e
dharmil25 <dharmil2524_shah@ymail.com>
2018-08-21 13:34:56 +0000

    Done

--- q04_mapping/build.py
+++ q04_mapping/build.py
@@ -1,10 +1,19 @@
 import pandas as pd
 import sys, os
 import numpy as np
-#sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q02_append_row.build import q02_append_row
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+from q02_append_row.build import q02_append_row
 def q04_mapping(path1,path2):
-    "write your solution here"
+    'write your solution here'
+    df_final= q02_append_row(path1)
+    scraped = pd.read_csv(path2)
+    scraped['United States of America'] = scraped['United States of America'].astype(str).apply(lambda x: x.lower())
+    scraped['US'] = scraped['US'].astype(str)
+    mapping = scraped.set_index('United States of America')['US'].to_dict()
+    df_final.insert(6, 'abbr', np.nan)
+    df_final['abbr'] = df_final['state'].map(mapping)
+    return df_final
+
 
 
 
