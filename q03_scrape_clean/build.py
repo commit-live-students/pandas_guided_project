0be638670587b8521079cf08c1a7342e75382932
sonik8494 <sourabh.niks1994@gmail.com>
2018-08-21 09:22:48 +0000

    Done

--- q03_scrape_clean/build.py
+++ q03_scrape_clean/build.py
@@ -1,9 +1,19 @@
+# %load q03_scrape_clean/build.py
 import pandas as pd
 import sys, os
 import requests
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 
-
+url = 'https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations'
 def q03_scrape_clean(url):
-    "write your solution here"
+
+    from urllib.request import urlopen
+
+    df = pd.read_html(url, skiprows = 11,header = 0)
+    df[0] = df[0].rename(columns = {'United States of America':'UnitedStatesofAmerica'})
+    data_folder = df[0].to_csv('scrapeddata.csv')
+    return df[0]
+
+q03_scrape_clean(url)
+
 
