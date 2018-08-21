cb52b9d57118e984f635abfd9c8187a81cda0516
sahusaurabh65 <sahusaurabh65@gmail.com>
2018-08-21 18:29:18 +0000

    Done

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -1,13 +1,25 @@
+# %load q02_append_row/build.py
 import pandas as pd
 import sys, os
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
-
-path = 'data/excel-comp-data.xlsx'
-
+path= 'data/excel-comp-data.xlsx'
 
 def q02_append_row(path):
-    "write your solution here"
+    'write your solution here'
+    df=pd.read_excel(path)
+    df['total'] = df['Jan']+df['Feb']+df['Mar']
+    df.loc[len(df),:]= df.sum()
+    print(df.head)
+    return(df)
+    
+    
+
+
+
+
+
+print(q02_append_row(path))
 
 
 
