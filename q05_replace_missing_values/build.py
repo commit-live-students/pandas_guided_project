5280dcda6bfa6e7e00cadf033a63cc2f31d9eec7
dharmil25 <dharmil2524_shah@ymail.com>
2018-08-21 16:59:45 +0000

    Done

--- q05_replace_missing_values/build.py
+++ q05_replace_missing_values/build.py
@@ -2,12 +2,19 @@ import pandas as pd
 import numpy as np
 import sys
 import os
-#sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q04_mapping.build import q04_mapping
+sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+from q04_mapping.build import q04_mapping
 
 path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 def q05_replace_missing_values(path1,path2):
-
+    'write your solution here'
+    df_final = q04_mapping(path1,path2)
+    df_mississipi = df_final[df_final['state'] == 'mississipi'].replace(np.nan, 'MS')
+    df_tenessee = df_final[df_final['state'] == 'tenessee'].replace(np.nan, 'TN')
+    df_final.replace(df_final.iloc[6], df_mississipi, inplace=True)
+    df_final.replace(df_final.iloc[10], df_tenessee, inplace=True)
+    return df_final
 
 #print(q05_replace_missing_values(path1,path2).shape)
+
