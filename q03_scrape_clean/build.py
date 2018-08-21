cc5eac46c99784a51c51584f0795b7e21c6f3f79
dharmil25 <dharmil2524_shah@ymail.com>
2018-08-21 13:21:01 +0000

    Done

--- q03_scrape_clean/build.py
+++ q03_scrape_clean/build.py
@@ -1,3 +1,4 @@
+# %load q03_scrape_clean/build.py
 import pandas as pd
 import sys, os
 import requests
@@ -5,5 +6,11 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 
 
 def q03_scrape_clean(url):
-    "write your solution here"
+    'write your solution here'
+    r = requests.get(url, auth=('user', 'pass'))
+    df = pd.DataFrame(pd.read_html(r.text)[0]).iloc[12:,:]
+    df.to_csv('scrapeddata.csv')
+    return df
+
+
 
