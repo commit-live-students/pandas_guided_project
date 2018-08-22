f64e2cdb5b332c889ffb518b70502777c16c143d
rajeshbrid <rajesh_brid@hotmail.com>
2018-08-22 03:10:24 +0000

    Done

--- q06_sub_total/build.py
+++ q06_sub_total/build.py
@@ -1,3 +1,4 @@
+# %load q06_sub_total/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -10,7 +11,22 @@ path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q06_sub_total(path1,path2):
-    "write your solution here"
+    # 'write your solution here'
+    df1 = pd.read_excel(path1)
+    df1['total']=df1['Jan']+df1['Feb']+df1['Mar']
+    df1.loc[len(df1),:]=df1.sum()
+    
+    df2 = pd.read_csv(path2)
+    
+    abbr_dict = dict(zip(df2.iloc[:,[1,6]]['United States of America'],df2.iloc[:,[1,6]]['Unnamed: 6']))
+    
+    df1.iloc[:,6]=df1['state'].map(abbr_dict)
+    df1.iloc[6,6] = 'MS'
+    df1.iloc[10,6] = 'TN'
+    
+    df1.rename(index=str,columns={'Jan':'abbr'},inplace=True)
+    
+    return df1.groupby(['abbr']).sum().iloc[:,:4]
 
 
 
