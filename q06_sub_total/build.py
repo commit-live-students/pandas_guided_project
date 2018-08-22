b0566d4b11b60c24b3b2fcd6ff69b9812bb64f03
rianboy18 <pranavr95.pn@gmail.com>
2018-08-22 14:39:04 +0000

    Done

--- q06_sub_total/build.py
+++ q06_sub_total/build.py
@@ -1,3 +1,4 @@
+# %load q06_sub_total/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -10,7 +11,32 @@ path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q06_sub_total(path1,path2):
-    "write your solution here"
+        df1=pd.read_excel(path1)
+        df1['total']=df1['Jan']+df1['Feb']+df1['Mar']
+        df1.loc[len(df1),:]=df1.sum()
+        df2=pd.read_csv(path2)
+        abbr_dict=dict(zip(df2.iloc[:,[1,6]]['United States of America'],df2['US']))#
+        f=list(df1['state'])
+        for i,j in enumerate(f):
+            if j=='Northcarolina':
+                f[i]='NorthCarolina'
+            elif j=='Mississipi':
+                f[i]='Mississippi'
+            elif j=='Rhodeisland':
+                f[i]='RhodeIsland'
+            elif j=='Tenessee':
+                f[i]='Tennessee'
+            elif j=='Northdakota':
+                f[i]='NorthDakota'
+        df1['state']=f
+        dftemp=df1['state'].map(abbr_dict)
+        df1.insert(loc=6,column='abbr',value=dftemp)
+        groupby_abbr_sum=df1.groupby('abbr')[['Jan','Feb','Mar','total']].sum()
+        return groupby_abbr_sum
 
+
+
+c=q06_sub_total(path1,path2)
+c
 
 
--- q07_symbols/build.py
+++ q07_symbols/build.py
@@ -1,18 +1,44 @@
+# %load q07_symbols/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import sys
 import os
-sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
+# sys.path.append(os.path.join(os.path.dirname(os.curdir)))
+# from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
 
 path1 = 'data/excel-comp-data.xlsx'
 path2 = 'data/scraped.csv'
 
 def q07_symbols(path1,path2):
-    "write your solution here"
+    df1=pd.read_excel(path1)
+    df1['total']=df1['Jan']+df1['Feb']+df1['Mar']
+    df1.loc[len(df1),:]=df1.sum()
+    df2=pd.read_csv(path2)
+    abbr_dict=dict(zip(df2.iloc[:,[1,6]]['United States of America'],df2['US']))#
+    f=list(df1['state'])
+    for i,j in enumerate(f):
+        if j=='Northcarolina':
+            f[i]='NorthCarolina'
+        elif j=='Mississipi':
+            f[i]='Mississippi'
+        elif j=='Rhodeisland':
+            f[i]='RhodeIsland'
+        elif j=='Tenessee':
+            f[i]='Tennessee'
+        elif j=='Northdakota':
+            f[i]='NorthDakota'
+    df1['state']=f
+    dftemp=df1['state'].map(abbr_dict)
+    df1.insert(loc=6,column='abbr',value=dftemp)
+    groupby_abbr_sum=df1.groupby('abbr')[['Jan','Feb','Mar','total']].sum()
+    return df1[['Jan','Feb','Mar']].applymap(lambda x:('$'+str(x)))
+    
 
 
 
-#print(q07_symbols(path1,path2))
+
+c=q07_symbols(path1,path2)
+c
+
 
