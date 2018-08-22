f10966ff6e34919bb6667db5be0eacd153b1accc
amagarwal-18 <amitagarwal.skit@gmail.com>
2018-08-22 19:05:30 +0000

    Done

--- q03_scrape_clean/build.py
+++ q03_scrape_clean/build.py
@@ -1,3 +1,4 @@
+# %load q03_scrape_clean/build.py
 import pandas as pd
 import sys, os
 import requests
@@ -5,5 +6,17 @@ sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 
 
 def q03_scrape_clean(url):
-    "write your solution here"
+    'write your solution here'
+    response = requests.get(url)
+    result = pd.read_html(response.text)
+    df = result[0][11:-1]
+    #df = df[0].str.strip()
+    df.to_csv('./data/scrapeddata.csv')
+    return df
+
+q03_scrape_clean('https://en.wikipedia.org/wiki/List_of_U.S._state_abbreviations')
+
+
+
+
 
