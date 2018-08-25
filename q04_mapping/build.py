22f4cb2d02ed635c77639d6f942bfb9023c14a00
Tusharshah2006 <tusharshah2006@gmail.com>
2018-08-25 03:45:16 +0000

    Done

--- q04_mapping/build.py
+++ q04_mapping/build.py
@@ -13,10 +13,11 @@ def q04_mapping(path1, path2):
     df_02.loc[len(df_02)] = df_02.iloc[:,6:10].sum(axis=0) 
     df_02.fillna(value=0, inplace=True, axis=0)
     
+    #df_02 = q02_append_row(path1)
     df_03 = pd.read_csv(path2)   
     mapping = dict(zip(df_03['United States of America'].str.lower(), df_03['Unnamed: 6']))
 
-    df_02.insert(loc=5, column='abbr', value='')
+    df_02.insert(loc=6, column='abbr', value='')
     df_02.iloc[:,6]=df_02['state'].map(mapping)
     
     return df_02 
