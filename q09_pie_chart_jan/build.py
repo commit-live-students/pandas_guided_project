c2392b3aa64584f5f332767a5e13fb5ba8ddffd0
EminentNeo <aniketjagtap18@gmail.com>
2018-08-26 10:47:59 +0000

    Done

--- q09_pie_chart_jan/build.py
+++ q09_pie_chart_jan/build.py
@@ -1,15 +1,37 @@
+# %load q09_pie_chart_jan/build.py
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import sys,os
 sys.path.append(os.path.join(os.path.dirname(os.curdir)))
-from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
+#from greyatomlib.pandas_guided_project.q06_sub_total.build import q06_sub_total
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
+path1 = 'data/excel-comp-data.xlsx'
+path2 = 'data/scraped.csv'
 
 def q09_pie_chart_jan(path1,path2):
+    df1 = pd.read_excel(path1)
+    df1['total']=df1['Jan']+df1['Feb']+df1['Mar']
+    df1.loc[len(df1),:]=df1.sum()
+    df2 = pd.read_csv(path2)
+    abbr_dict = dict(zip(df2.iloc[:,[1,6]]['United States of America'],df2.iloc[:,[1,6]]['Unnamed: 6']))
+    df1['abbr']=df1['state'].map(abbr_dict)
+    #df1.drop(df1.columns[[6,7,]], axis=1,inplace=True)
+    #df1.loc[df1[6,6].fillna('MS',inplace=True)
+    list1 = ['MSS','TEN','none']
+    df1['abbr']=df1['abbr'].fillna('MS',limit=1)
+    df1['abbr']=df1['abbr'].fillna('TN',limit=1)
+    df1['abbr']=df1['abbr'].fillna('None',limit=1)
+    df1 = df1.drop(df1.index[len(df1)-1])
+    df =df1.groupby('abbr')['Jan','Feb','Mar','total'].sum()
+    plot = df.plot.pie(y='Jan', figsize=(5, 5))
+    return plot
 
-    "write your solution here"
+
+q09_pie_chart_jan(path1,path2)
+
+
 
 
 
