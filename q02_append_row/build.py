d890f4dcd364ee8a689ed285b69218ce154b8b7d
Sarang Narkhede <narkhedesarang@gmail.com>
2019-04-06 16:49:46 -0400

    Done

--- q02_append_row/build.py
+++ q02_append_row/build.py
@@ -3,10 +3,15 @@ import sys, os
 #sys.path.append(os.path.join(os.path.dirname(os.curdir)))
 from greyatomlib.pandas_guided_project.q01_load_data.build import q01_load_data
 
-
 def q02_append_row(path):
-    "write your solution here"
-
-
+    df = q01_load_data(path)
+    df1 = df[['Jan', 'Feb', 'Mar', 'total']].sum(axis=0)
+    df1 = pd.DataFrame(df1)
+    df2 = pd.DataFrame(columns=['Jan', 'Feb', 'Mar', 'total'])
+    df2['Jan'] = df1.loc['Jan']
+    df2['Feb'] = df1.loc['Feb']
+    df2['Mar'] = df1.loc['Mar']
+    df2['total'] = df1.loc['total']
+    df3 = df.append(df2, ignore_index=True)
 
 
